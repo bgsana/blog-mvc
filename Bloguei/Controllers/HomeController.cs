333bc8d0440f640c057c5287e735e45e0a2f63fe
bgsana <bgs.ana08@gmail.com>
2025-08-08 20:32:33 -0300

    2º Commit - Criamos as classes para o nosso blog

--- Bloguei/Controllers/HomeController.cs
+++ Bloguei/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ public class HomeController : Controller
     // Cria um objeto chamado logger que é somente de leitura. 
     private readonly ILogger<HomeController> _logger;
 
-    //Método construtor: cria objeto na memória
+    // Método construtor: cria objeto na memória
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
@@ -18,6 +18,30 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewData["Mensagem"] = "Divas!";
+        // Criar objetos
+        Categoria categoria = new();
+        categoria.Id = 1;
+        categoria.Nome = "Closet dos Sonhos";
+
+        Categoria categoria2 = new()
+        {
+            Id = 2,
+            Nome = "Glow Up"
+        };
+
+        List<Postagem> postagens = [
+            new(){
+                Id = 1,
+                Nome = "Seja a nova Malu Borges com essas super tendências!",
+                CategoriaId = 1,
+                Categoria = categoria,
+                DataPostagem = DateTime.Parse("08/08/2025"),
+                Descricao = "Labubu",
+                Texto = "Lorem ipsum"
+            }
+        ];
+
         return View();
     }
 
